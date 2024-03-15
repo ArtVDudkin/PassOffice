6157e77b25a1626781bcc9e83861bbf62fe51e52
Artem <kardan97@yandex.ru>
2024-03-15 09:04:52 +0700

    Spring project structure generated

---
+++ passoffice/src/main/java/ru/gb/passoffice/PassOfficeApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.passoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassOfficeApplication.class, args);
+	}
+
+}
