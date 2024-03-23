01e19e4a59bae227fb95b651ab0e65a876b34876
Artem <kardan97@yandex.ru>
2024-03-23 20:31:16 +0700

    Microservice strusture added

---
+++ pass-office/pass-service/src/main/java/ru/gb/PassServiceApplication.java
@@ -0,0 +1,11 @@
+package ru.gb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PassServiceApplication.class, args);
+    }
+}
